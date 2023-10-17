54aa288e31b3bf9773bf1d24efee08c028f72f6d
Igor <34138855+igorbrasan@users.noreply.github.com>
2023-10-17 08:11:08 -0300

    Update gitignore

--- src/main/java/com/mycompany/schoolbus/Endereco.java
+++ src/main/java/com/mycompany/schoolbus/Endereco.java
@@ -25,39 +25,67 @@ public class Endereco
         this.bairro = bairro;
         this.numero = numero;
     }
-
+    /**
+     * Get value of Numero.
+     * @return 
+     */
     public int getNumero() {
         return numero;
     }
-
+    /**
+     * Setter pro número da casa, prédio ou terreno em Endereço
+     * @param numero 
+     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
-
+    /**
+     * Retorna o nome da rua.
+     * @return 
+     */
     public String getNomeDaRua() {
         return nomeDaRua;
     }
-
+    /**
+     * Seta o nome da rua.
+     * @param nomeDaRua 
+     */
     public void setNomeDaRua(String nomeDaRua) {
         this.nomeDaRua = nomeDaRua;
     }
-
+    /**
+     * Retorna o valor do Complemento. 
+     * @return 
+     */
     public String getComplemento() {
         return complemento;
     }
-
+    /**
+     * Seta o valor Complemento. Exemplos: Proximo ao mercado X; Bloco z apartamento j; ao lado da farmácia k...
+     * @param complemento 
+     */
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
-
+    /**
+     * Getter para o Bairro
+     * @return 
+     */
     public String getBairro() {
         return bairro;
     }
-
+    /**
+     * Setter para o Bairro
+     * @param bairro 
+     */
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
-
+    /**
+     * Concatena os valores: @nomeDaRua, @numero, @bairro, e @complemento, e os retorna em uma só string. 
+     * Criado para facilitar consultas por classes que tenham um Endereço.
+     * @return 
+     */
     public String toString(){
         String end = (nomeDaRua + ", " + numero + ", " + bairro + ", " + complemento);
         return end;
