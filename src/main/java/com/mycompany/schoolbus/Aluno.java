239d56c89d657117fa955dfb5f640d1971633d4b
Igor <34138855+igorbrasan@users.noreply.github.com>
2023-10-20 08:19:05 -0300

    Included param PontoDeparada, remove param rota

--- src/main/java/com/mycompany/schoolbus/Aluno.java
+++ src/main/java/com/mycompany/schoolbus/Aluno.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Aluno extends PessoaFisica
 {
     public Escola escola;
-    public Rota rota;
+    public PontoDeParada pontoDeParada;
     private String matricula, serie;
 
     /**
@@ -55,35 +55,61 @@ public class Aluno extends PessoaFisica
         this.serie = serie;
     }
 
+    /**
+     * 
+     * @return
+     */
     public Escola getEscola() {
         return escola;
     }
 
+    /**
+     * 
+     * @param escola
+     */
     public void setEscola(Escola escola) {
         this.escola = escola;
     }
-
-    public Rota getRota() {
-        return rota;
-    }
-
-    public void setRota(Rota rota) {
-        this.rota = rota;
-    }
-
+    /**
+     * 
+     * @return
+     */
     public String getMatricula() {
         return matricula;
     }
-
+    /**
+     * 
+     * @param matricula
+     */
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
-
+/**
+ * 
+ * @return
+ */
     public String getSerie() {
         return serie;
     }
-
+/**
+ * 
+ * @param serie
+ */
     public void setSerie(String serie) {
         this.serie = serie;
+    }
+/**
+ * 
+ * @return
+ */
+    public PontoDeParada getPontoDeParada() {
+        return pontoDeParada;
+    }
+/**
+ * 
+ * @param pontoDeParada
+ */
+    public void setPontoDeParada(PontoDeParada pontoDeParada) {
+        this.pontoDeParada = pontoDeParada;
     }
 }
